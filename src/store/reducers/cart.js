f66f255502adfeea10e03c9531b9baf82610a20b
jorgehaf <jorge.ferreira@direcional.com.br>
2022-02-09 14:05:57 -0300

    Mudanças e melhorias no redux

--- src/store/reducers/cart.js
+++ src/store/reducers/cart.js
@@ -1,8 +1,5 @@
-import productList from './product';
-
 const INITIAL_STATE = {
     value: 0,
-    products: productList,
     Cart: []
 }
 
@@ -10,27 +7,31 @@ export default function cart(state = INITIAL_STATE, action) {
     switch (action.type) {
         case 'ADD_TO_CART':
             console.log('ADD_TO_CART')
-            console.log(action)
+            console.log('state', state, 'action', action)
             return state;
 
 
         case 'ADD_ITEM':
             console.log('ADD_ITEM')
+            console.log('state', state, 'action', action)
             return state;
 
 
         case 'REMOVE_ITEM':
-            console.log('entrei')
+            console.log('REMOVE_ITEM')
+            console.log('state', state, 'action', action)
             return state;
 
 
         case 'DELETE_ITEM':
             console.log('DELETE_ITEM')
+            console.log('state', state, 'action', action)
             return state;
 
 
         case 'CHANGE_CART':
             console.log('CHANGE_CART')
+            console.log('state', state, 'action', action)
             return state;
 
 
